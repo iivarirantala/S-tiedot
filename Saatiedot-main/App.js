538ae54fe36ed55732934696b93c1bc000fa3435
iivarirantala <112494494+iivarirantala@users.noreply.github.com>
2024-03-28 20:02:37 +0200

    Add files via upload

---
+++ Saatiedot-main/App.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Position from './components/Position'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Current Weather</Text>
+      <Position />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading: {
+
+  },
+});
